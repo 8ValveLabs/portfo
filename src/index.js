66cc66eee9a8f5ddec3ee31772a496dd9440139d
nashm <nash.michel@hotmail.com>
2022-07-13 16:27:30 -0400

    Totally Normal Commit just fixing spelling mistakes.

--- src/index.js
+++ src/index.js
@@ -10,11 +10,10 @@ import ParallaxComponent from './stateless/ParallaxComponent';
 const root = ReactDOM.createRoot(
     document.getElementById('root')
 );
-const topNavItms = ["Cunt","Shit","Fuck"];
 
 root.render(
     <div>
-        <Banner text={topNavItms}></Banner>
+        <Banner></Banner>
         <ParallaxBackground pid=''>
             <ParallaxComponent>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
